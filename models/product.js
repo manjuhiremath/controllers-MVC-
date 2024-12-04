2471dad27c307d016a2adcb99417c578bb772920
manjuhiremath <manjuhiremath1352@gmail.com>
2024-12-04 17:29:51 +0530

    model

--- models/product.js
+++ models/product.js
@@ -1,13 +1,35 @@
-const products = []
-module.exports = class Product{
-    constructor(title){
+const fs = require('fs');
+const path = require('path');
+const p = path.join(path.dirname(process.mainModule.filename),
+        'data',
+        'products.json'
+    );
+const getProductsFromFile = cb => {
+    
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+           return cb([]);
+        }else{
+
+        }
+        cb(JSON.parse(fileContent));
+    });
+}
+
+module.exports = class Product {
+    constructor(title) {
         this.title = title;
     }
-    save(){
-        products.push(this);
+    save() {
+        getProductsFromFile(products=>{
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            })
+        });
     }
 
-    static fetchAll(){
-        return products;
+    static fetchAll(cb) {
+        getProductsFromFile(cb)
     }
 }
